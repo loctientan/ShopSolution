3aba0bc1e034c4f8c01d8337d044f7bcb47dfcd8
TienTanLoc <loctientan@gmail.com>
2020-10-01 15:33:51 +0700

    Add project files.

---
+++ WebShopSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebShopSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
